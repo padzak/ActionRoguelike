d5c48c21ab3e94c79b22ccdadad1c3327dd29859
padzak <mrcn.pajak@gmail.com>
2025-01-26 19:36:23 +0100

    Added follow controller rotation programmatically in comments

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -1,9 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#include "DrawDebugHelpers.h"
-
 #include "SCharacter.h"
 
+#include "DrawDebugHelpers.h"
+
+
 #include "Camera/CameraComponent.h"
+//#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -12,11 +14,22 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
-	SpringArmComp->SetupAttachment(RootComponent);
+SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+    SpringArmComp->SetupAttachment(RootComponent);
 
-	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
-	CameraComp->SetupAttachment(SpringArmComp);
+    // Enable the spring arm to use the controller rotation
+    SpringArmComp->bUsePawnControlRotation = true;
+
+    CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+    CameraComp->SetupAttachment(SpringArmComp);
+
+    // Prevent character rotation from affecting the camera
+    //bUseControllerRotationPitch = false;
+    //bUseControllerRotationYaw = true;
+    //bUseControllerRotationRoll = false;
+
+    // Allow the character to orient based on movement
+    //GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
@@ -54,9 +67,9 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("MoveRight",   this, &ASCharacter::MoveRight);
 	
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Turn",	 this, &APawn::AddControllerYawInput);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
