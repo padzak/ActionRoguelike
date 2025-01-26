74a284befcfc6d28e5b0f1bedcdc1b2eb4d6a661
padzak <mrcn.pajak@gmail.com>
2025-01-26 20:03:27 +0100

    Added magic projectile c++ class

---
+++ Source/ActionRoguelike/SMagicProjectile.cpp
@@ -0,0 +1,20 @@
+#include "SMagicProjectile.h"
+
+// Sets default values
+ASMagicProjectile::ASMagicProjectile()
+{
+	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ASMagicProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ASMagicProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/ActionRoguelike/SMagicProjectile.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SMagicProjectile.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASMagicProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ASMagicProjectile();
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* SphereComp;
+	
+	UPROPERTY(VisibleAnywhere)
+	UProjectileMovementComponent* MovementComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* EffectComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
