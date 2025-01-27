a738f528f2ad1b178badf5d9a753fcc7cdc8a8c4
padzak <mrcn.pajak@gmail.com>
2025-01-27 16:43:07 +0100

    Added ItemChest class

---
+++ Source/ActionRoguelike/SItemChest.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SItemChest.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASItemChest : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* LidMesh;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Sets default values for this actor's properties
+	ASItemChest();
+
+};
