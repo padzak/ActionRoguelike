076cb5da52399432dee74684f52bdeab5c6f56ae
padzak <mrcn.pajak@gmail.com>
2025-02-04 22:21:49 +0100

    Added health update on damage taken

--- Source/ActionRoguelike/Public/SAttributeComponent.h
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "SAttributeComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHealthChanged, AActor*, InstigatorActor, USAttributeComponent*, OwningComp, float, NewHealth, float, Delta);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
@@ -33,6 +34,9 @@ protected:
 
 
 public:
+	UPROPERTY(BlueprintAssignable, Category="Attributes")
+	FOnHealthChanged OnHealthChanged;
+
 	UFUNCTION(BlueprintCallable, Category="Attributes")
 	bool ApplyHealthChange(float Delta);
 };
